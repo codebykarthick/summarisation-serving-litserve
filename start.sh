1091593d22bcea81fbdb03ca64d54bd85caf8264
Sri Hari Karthick <sriharikarthik2641999@gmail.com>
2025-06-02 11:00:31 +0100

    Updated start script.

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
-# While this one line command can be directly given as the startup command to Docker container
-# This allows for more flexibility such as loading secrets, etc to be passed along if the need
-# arises.
+#!/bin/bash
+set -e
+
+# Allows future additions like secrets/env config
 python -m app.main --port 8080 --batch 16
